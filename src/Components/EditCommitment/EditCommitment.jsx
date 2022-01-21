2fb626a4e7e5c10d32aeb3434c83b39751aefac4
Emma <emmarraguila@comunidad.unam.mx>
2022-01-21 10:08:44 -0600

    build: table

--- src/Components/EditCommitment/EditCommitment.jsx
+++ src/Components/EditCommitment/EditCommitment.jsx
@@ -1,11 +1,15 @@
 import NavBar from "./NavBar";
 import EditTable from "./EditTable/EditTable";
+import { Box } from "@mui/material";
 
 export default function EditCommitment() {
   return (
-    <>
+    <Box
+      component="main"
+      sx={{ backgroundColor: "#E0E2E9", minHeight: "100vh", p: 1 }}
+    >
       <NavBar />
       <EditTable />
-    </>
+    </Box>
   );
 }
--- src/Components/EditCommitment/EditTable/EditTable.jsx
+++ src/Components/EditCommitment/EditTable/EditTable.jsx
@@ -1,12 +1,20 @@
-import Table from "@mui/material/Table";
-import Paper from "@mui/material/Paper";
+import {
+  Box,
+  Fab,
+  Table,
+  Paper,
+  Input,
+  FormControl,
+  InputLabel,
+  TableContainer,
+} from "@mui/material";
 import EditTableRow from "./EditTableRow";
 import EditTableHead from "./EditTableHead";
-import TableContainer from "@mui/material/TableContainer";
+import React, { useState } from "react";
 
 export default function EditTable() {
   // Se recibirá de firestore, esto es sólo un mock
-  
+
   const actionTableInfo = [
     {
       "SMART-Goal": "Learn time management tool",
@@ -21,17 +29,69 @@ export default function EditTable() {
   const headValues = Object.keys(actionTableInfo[0]);
   const rows = actionTableInfo.map((obj) => Object.values(obj));
 
+/*   const [newData, useNewData] = useState([]);
+  const [input1, useInput1] = useState();
+  const [input2, useInput2] = useState();
+  const [input3, useInput3] = useState();
+
+  const addData = (e) => {
+    e.preventDefault();
+    useNewData([]);
+    actionTableInfo.push(newData);
+  }; */
+
   return (
     <>
       <TableContainer
         component={Paper}
-        sx={{ width: "80%", margin: "auto", marginTop: "1em" }}
+        sx={{ width: 1 / 3, mx: "auto", my: "1em" }}
       >
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table sx={{ width: 7 / 8, m: 2 }} aria-label="simple table">
           <EditTableHead headValues={headValues} />
           <EditTableRow rows={rows} />
         </Table>
       </TableContainer>
+      <Box
+        sx={{
+          width: 1 / 3,
+          display: "flex",
+          flexWrap: "no-wrap",
+          mx: "auto",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          component="section"
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          {headValues.map((input) => (
+            <>
+              <FormControl sx={{ width: 4 / 9, minWidth: "10em" }}>
+                <InputLabel htmlFor={`${input}-label`}>{input}</InputLabel>
+                <Input
+                  id={`${input}-label`}
+                  type="text"
+                  aria-label={input}
+                  /* onChange={(e) => `${setInput(e.target.value)}`} */
+                />
+              </FormControl>
+            </>
+          ))}
+        </Box>
+        <Fab
+          color="primary"
+          aria-label="add"
+          className="material-icons"
+          sx={{ width: "3em", height: "3em" }}
+          /* onClick={(e) => addData(e)} */
+        >
+          +
+        </Fab>
+      </Box>
     </>
   );
 }
