f42bf70cb32dc11d5ad81c37a071be2ce7efe66d
Emma <emmarraguila@comunidad.unam.mx>
2022-01-21 16:55:43 -0600

    build: changes tables

--- src/Components/EditCommitment/EditCommitment.jsx
+++ src/Components/EditCommitment/EditCommitment.jsx
@@ -1,17 +1,41 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import NavBar from "./NavBar";
-import EditTable from "./EditTable/EditTable";
 import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import EditTable from "./EditTable/EditTable";
+import { db } from "../../Firebase/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function EditCommitment() {
-  const titles =["focus", "plan", "practice", "accountablity", "log"]
+  const [data, setData] = useState();
+  const [newData, setNewData] = useState({});
+
+  // Trial values
+  const titles = ["focus", "plan", "practice", "accountablity", "log"];
+
+  const firestore = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data())
+        setData(doc.data());
+        return doc.data()
+      });
+    } catch (err) {
+      console.log(err);
+      return(err)
+    }
+  };
+
+  useEffect(() => {
+    firestore();
+  }, []);
+
   return (
-    <Box
-      component="main"
-      sx={{ backgroundColor: "#E0E2E9", minHeight: "100vh", p: 1 }}
-    >
+    <Box component="main">
       <NavBar />
       {titles.map((title, index) => (
-        <EditTable key={title + index} title={title}/>
+        <EditTable key={title + index} title={title} data={data} setNewData={setNewData} />
       ))}
     </Box>
   );
