398c42601ca8596690e997e2ae4b56e165d59d70
Emma <emmarraguila@comunidad.unam.mx>
2022-01-24 09:30:32 -0600

    build: get data dinamically

--- src/Components/EditCommitment/EditCommitment.jsx
+++ src/Components/EditCommitment/EditCommitment.jsx
@@ -1,33 +1,69 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import NavBar from "./NavBar";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import EditTable from "./EditTable/EditTable";
 import { useDataContext } from "../../Hooks/json-provider";
 
 export default function EditCommitment() {
-  const { data, selectedPeriod } = useDataContext();
-  const [periodData, setPeriodData] = useState();
+  const { data, selectedPeriod, currentUser, currentAmbition } =
+    useDataContext();
+  const link = "http://localhost:5000/";
+  const endpoints = [
+    "Career-Ambitions",
+    "Period",
+    "Main-Goal",
+    "Areas-Of-Focus",
+    "Action-Plan",
+    "Accountability",
+    "Planned-Deliberate-Practices",
+    "Deliberate-Practice-Log",
+  ];
 
-  /* useEffect(() => {
-    if (data) {
-      setPeriodData(data);
+  useEffect(() => {
+    if (data && data !== {}) {
     }
-  }, [data]); */
-  console.log(data.accountability[0]);
+  }, [data]);
 
   return (
     <Box component="main">
       <NavBar />
-      {periodData
-        ? Object.keys(periodData).map((title, index) => (
-            <EditTable
-              key={title + index}
-              title={title}
-              data={periodData}
-              setNewData={setPeriodData}
-            />
-          ))
+      {data ? (
+        <>
+          <Typography>
+            Main Goal:{" "}
+            {data
+              ? data.mainGoal.filter(
+                // something doesn't let it render all the times
+                // add current user filter
+                  (info) => info.ambition === currentAmbition
+                )[0].name
+              : null}
+          </Typography>
+          <Typography>
+            Period:{" "}
+            {data
+              ? data.period.filter(
+                  (data) => data.ambition === currentAmbition
+                )[0].id
+              : null}
+          </Typography>
+        </>
+      ) : null}
+      {data
+        ? Object.keys(data).map((title, index) =>
+            title !== "period" &&
+            title !== "mainGoal" &&
+            title !== "ambition" &&
+            title !== "users" ? (
+              <EditTable
+                key={title + index}
+                title={endpoints[index]}
+                tableData={data[title]}
+                selectedPeriod={selectedPeriod}
+              />
+            ) : null
+          )
         : null}
     </Box>
   );
