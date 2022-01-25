736817e4e6dac3f8d852c54bc0e9a6758baaa341
Nancy <nancy.090495@gmail.com>
2022-01-24 21:09:02 -0600

    new format in calendar

--- src/Components/Dashboard/SideBar/Calendar/Index.jsx
+++ src/Components/Dashboard/SideBar/Calendar/Index.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
@@ -20,18 +21,42 @@ const Calendars = () => {
     /* const { data, activitiesDay } = useDataContext();
     console.log(data.practiceLog[0].Date)
     console.log(data.practiceLog[0].period) */
-    
-   
+    const [data, setData] = useState([]);
+
+  let url = "http://localhost:5000/Deliberate-Practice-Log" // campo "id"
+  // let urlOne = "http://localhost:5000/Main-Goal" // campo "name"
+  // let urlTwo = "http://localhost:5000/Action-Plan"; // campo "status"
+
+
+  const fetchApi = async () => {
+    const response =await fetch(url)
+    const responseJSON= await response.json();
+    setData(responseJSON)
+    //console.log(responseJSON[0].Date)
+  }
+
+  useEffect (() => {
+    fetchApi()
+  }, [])
+
+  //Fecha desde json
+    // const date = data[0].Date;
+    // const newDate = new Date (date);
+    // console.log(newDate)
+    // const dayDate = newDate.getDate();
+    // const monthDate = newDate.getMonth()+1;
+    // const yearDate = newDate.getFullYear();
+
     /*** HOOKS ***/
     const [dateObject, setdateObject] = useState(moment());
     const [showMonthTable, setShowMonthTable] = useState(false);
     const [selectedDay, setSelected] = useState(defaultSelectedDay);
-    
+
     /*** CALENDAR HEAD ***/
     const allMonths = moment.months();
     const currentMonth = () => dateObject.format("MMMM");
     const currentYear = () => dateObject.format("YYYY");
-    
+
 
     const setMonth = month => {
         let monthNo = allMonths.indexOf(month);
@@ -40,7 +65,7 @@ const Calendars = () => {
         setdateObject(newDateObject);
         setShowMonthTable(false);
     }
-    
+
     const toggleMonthSelect = () => setShowMonthTable(!showMonthTable);
     /*** CALENDAR BODY ***/
     const setSelectedDay = day => {
@@ -54,15 +79,18 @@ const Calendars = () => {
     const currentMonthNum = () => dateObject.month();
     const daysInMonth = () => dateObject.daysInMonth();
     const currentDay = () => dateObject.format("D");
-    
+
     const actualMonth = () => moment().format("MMMM");
 
     const firstDayOfMonth = () => moment(dateObject).startOf("month").format("d");
 
+
+
     const retrieveData = () => {
         let queryDate = `${selectedDay.day}-${selectedDay.month}-${selectedDay.year}`;
-        console.log(queryDate)
+
     }
+    //useEffect(() => retrieveData(), [selectedDay]);
 
   return <div className='calendarInfo'>
       <SideBar></SideBar>
@@ -76,7 +104,7 @@ const Calendars = () => {
                         showMonthTable={showMonthTable}
                         toggleMonthSelect={toggleMonthSelect}
                     />
-                    <CalendarBody 
+                    <CalendarBody
                         firstDayOfMonth={firstDayOfMonth}
                         daysInMonth={daysInMonth}
                         currentDay={currentDay}
@@ -85,13 +113,22 @@ const Calendars = () => {
                         actualMonth={actualMonth}
                         setSelectedDay={setSelectedDay}
                         selectedDay={selectedDay}
-                        weekdays={moment.weekdays()} 
-                        
+                        weekdays={moment.weekdays()}
+
                     />
         </Grid>
         <Grid item xs={12} md={7}>
                 <Paper className="paper-activities">
                 <h3>Activities on {selectedDay.day}-{selectedDay.month + 1}</h3>
+                {/* <ActivityList
+                    loading={loading}
+                    activities={activities}
+                    //authUser={props.authUser}
+                    //setOpenSnackbar={setOpenSnackbar}
+                    //setSnackbarMsg={setSnackbarMsg}
+                    //editActivity={editActivity}
+                    //setEditing={setEditing}
+                /> */}
                 </Paper>
         </Grid>
         </div>
